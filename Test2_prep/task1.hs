081215ca4d01b18297c9b6825ee184ae761d1085
sadjokerwin <al107ivanov@gmail.com>
2023-01-12 00:23:30 +0200

    Create task1.hs

---
+++ Test2_prep/task1.hs
@@ -0,0 +1,14 @@
+helperSqDig :: Int -> [Int]
+helperSqDig a 
+    | a < 10 = [rem a 10 * rem a 10]
+    |otherwise = rem a 10 * rem a 10 : helperSqDig (div a 10)
+
+listToNum :: [Int] -> Int
+listToNum [a] = a
+listToNum (a:num) = a*10^(length num + 1) + listToNum num
+
+squareDigits :: Int -> Int
+squareDigits a = if a > 0 then listToNum (helperSqDig a) else (-1)*listToNum (helperSqDig (modOfNum a))
+
+modOfNum :: Int -> Int
+modOfNum a = a*(-1)
